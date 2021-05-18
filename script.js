b74cc54227e6d150196d84845efdbd46a133a5e7
putra-55 <aryainggil3@gmail.com>
2021-05-18 19:28:21 +0700

    completing about who am i

--- script.js
+++ script.js
@@ -10,10 +10,34 @@ toggleMenu.addEventListener("click", function () {
 // typed introduction
 var typed = new Typed(".text-typed", {
   // Waits 1000ms after typing "First"
-  strings: ["Web Developer.", "Freelancer.", "Blogger."],
+  strings: ["Full Stack Developer.", "Web Developer.", "Freelancer."],
   loop: true,
   typeSpeed: 100,
   delaySpeed: 200,
   backSpeed: 100,
   startDelay: 500,
+});
+
+// check section
+// give background violet for odd section
+const sections = document.querySelectorAll("section");
+sections.forEach((section, i) => {
+  if ((i + 1) % 2 == 1) {
+    section.classList.add("violet");
+  }
+});
+
+// give active menu if clicked
+const containerMenu = document.querySelector(".my-menu");
+const listMenu = document.querySelectorAll(".link-menu");
+
+containerMenu.addEventListener("click", function (e) {
+  // alert("ok");
+  console.log(e.target);
+  if (e.target.className == "link-menu") {
+    listMenu.forEach((menu) => {
+      menu.className = "link-menu";
+    });
+    e.target.classList.add("active");
+  }
 });
